69430b73db761dbf62c45b412f7b70f3adcd553b
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-01-26 19:25:35 +0100

    update store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import store from './app/store';
 import Footer from './components/Footer';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
     <Footer/>
   </Provider>
