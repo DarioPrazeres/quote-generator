a1fb2a368a8fdf8f38b5355ee4de7ffb9868ee3b
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-01-26 19:21:37 +0100

    update project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+export default function App(){
     return (
       <div className="App">
         <h1>Dario Prazeres</h1>
       </div>
     );
-  }
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import Footer from './components/Footer';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Provider>
+    <App />
+    <Footer/>
+  </Provider>
+)
